62793480900ab2437138af132402ce147b2a6ca8
NIKESH S <nikesh06042004@gmail.com>
2026-01-30 14:52:58 +0530

    Udated configuration if vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: [
+      'jgy6qd2ypvrd.share.zrok.io'
+    ],
     proxy: {
       '/api': 'http://localhost:8888',
       '/uploads': 'http://localhost:8888'
